560bd86ccd8e397a3c7b2a4162e44c17d13b6b43
Mason <mason.pamarang586@myci.csuci.edu>
2025-10-22 21:31:40 -0700

    Added character interface with base functions as well as a player and enemy class to implement and set to base attributes

---
+++ core/src/main/kotlin/com/anotherround/CharacterClasses/Character.kt
@@ -0,0 +1,30 @@
+package com.anotherround.CharacterClasses
+
+interface Character {
+    //Stats
+    val name: String
+    var level: Int
+    var health: Int
+    var defenseStat: Int
+    var attackStat: Int
+
+    // functions
+    fun defend(incomingDamage: Int) { // this could have some flaws
+        health -= (defenseStat - incomingDamage)
+    }
+    fun attack(target: Character) {
+        target.takeDamage(attackStat)
+        println("$name attacks ${target.name} for $attackStat damage!")
+    }
+    fun takeDamage(incomingDamage: Int) {
+        health -= incomingDamage
+        println("$name takes $incomingDamage damage. Remaining health: $health")
+    }
+    fun heal(amount: Int) {
+        health += amount
+        println("Healed $amount hp")
+    }
+    fun isAlive(): Boolean {
+        return health > 0
+    }
+}
---
+++ core/src/main/kotlin/com/anotherround/CharacterClasses/Player.kt
@@ -0,0 +1,9 @@
+package com.anotherround.CharacterClasses
+
+class Player (
+    override val name: String,
+    override var level: Int = 1,
+    override var health: Int = 100,
+    override var defenseStat: Int = 1,
+    override var attackStat: Int = 10
+) : Character {}
