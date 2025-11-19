47c0b775b8f7d665ccf39dfe47c9c4c75e21e51c
Mason <mason.pamarang586@myci.csuci.edu>
2025-11-19 11:14:07 -0800

    added more assets to the project

--- core/src/main/kotlin/com/anotherround/CharacterClasses/Enemies.kt
+++ core/src/main/kotlin/com/anotherround/CharacterClasses/Enemies.kt
@@ -1,6 +1,6 @@
 package com.anotherround.CharacterClasses
 
-class Enemies(
+class RedEnemy(
     override val name: String = "BadGuy",
     override var level: Int = 1,
     override var health: Int = 20,
@@ -8,4 +8,14 @@ class Enemies(
     override var attackStat: Int = 10
 ) : Character {
     override val maxHealth: Int get() = 20
+}
+
+class Phantom(
+    override val name: String = "Phantom",
+    override var level: Int = 2,
+    override var health: Int = 35,
+    override var defenseStat: Int = 2,
+    override var attackStat: Int = 13
+) : Character {
+    override val maxHealth: Int get() = 35
 }
