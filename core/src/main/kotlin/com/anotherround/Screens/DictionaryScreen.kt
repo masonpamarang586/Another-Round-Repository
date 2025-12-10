7181332fb287866cc30b31ec3f036b18a07bec61
Oliviah <oliviah.weissman130@myci.csuci.edu>
2025-12-10 02:27:35 -0800

    Finished the full dictionary screen.

--- core/src/main/kotlin/com/anotherround/Screens/DictionaryScreen.kt
+++ core/src/main/kotlin/com/anotherround/Screens/DictionaryScreen.kt
@@ -88,44 +88,61 @@ class DictionaryScreen(val game: Main) : KtxScreen {
         //Grunt
         val entry_Grunt = Image(Texture((Gdx.files.internal("generic_char_v0.2/png/red/char_red_1_index10.png"))))
         val entryTitle_Grunt = Image(Texture((Gdx.files.internal("ui/entryTitle_Grunt.png"))))
+        val entryStats_Grunt = Image(Texture((Gdx.files.internal("ui/entryStats_Grunt.png"))))
         stage.addActor(entry_Grunt)
         entry_Grunt.setSize(500f, 500f)
         entry_Grunt.setPosition(50f, 1400f)
         stage.addActor(entryTitle_Grunt)
         entryTitle_Grunt.setSize(550f, 200f)
         entryTitle_Grunt.setPosition(70f, 1750f)
+        stage.addActor(entryStats_Grunt)
+        entryStats_Grunt.setSize(250f, 250f)
+        entryStats_Grunt.setPosition(230f, 1250f)
 
         //Phantom
         val entry_Phantom = Image(Texture((Gdx.files.internal("phantom/phantomIdleFrame1.png"))))
         val entryTitle_Phantom = Image(Texture((Gdx.files.internal("ui/entryTitle_Phantom.png"))))
+        val entryStats_Phantom = Image(Texture((Gdx.files.internal("ui/entryStats_Phantom.png"))))
         stage.addActor(entry_Phantom)
         entry_Phantom.setSize(500f, 500f)
         entry_Phantom.setPosition(520f, 1450f)
         stage.addActor(entryTitle_Phantom)
         entryTitle_Phantom.setSize(550f, 200f)
         entryTitle_Phantom.setPosition(490f, 1870f)
+        stage.addActor(entryStats_Phantom)
+        entryStats_Phantom.setSize(300f, 250f)
+        entryStats_Phantom.setPosition(600f, 1250f)
 
         //Evil Wizard
         val entry_EvilWizard = Image(Texture((Gdx.files.internal("evil_wizard/evilWizardIdleFrame1.png"))))
         val entryTitle_EvilWizard = Image(Texture((Gdx.files.internal("ui/entryTitle_EvilWizard.png"))))
+        val entryStats_EvilWizard = Image(Texture((Gdx.files.internal("ui/entryStats_EvilWizard.png"))))
         stage.addActor(entry_EvilWizard)
         entry_EvilWizard.setSize(500f, 500f)
-        entry_EvilWizard.setPosition(120f, 470f)
+        entry_EvilWizard.setPosition(120f, 520f)
         stage.addActor(entryTitle_EvilWizard)
         entryTitle_EvilWizard.setSize(550f, 200f)
-        entryTitle_EvilWizard.setPosition(110f, 950f)
+        entryTitle_EvilWizard.setPosition(110f, 1000f)
+        stage.addActor(entryStats_EvilWizard)
+        entryStats_EvilWizard.setSize(300f, 250f)
+        entryStats_EvilWizard.setPosition(250f, 240f)
 
 
 
         //Night Borne
         val entry_NightBorne = Image(Texture((Gdx.files.internal("nightborne/NightBorneIdleFrame1.png"))))
         val entryTitle_NightBorne = Image(Texture((Gdx.files.internal("ui/entryTitle_Nightborne.png"))))
+        val entryStats_NightBorne = Image(Texture((Gdx.files.internal("ui/entryStats_Nightborne.png"))))
         stage.addActor(entry_NightBorne)
         entry_NightBorne.setSize(500f, 500f)
-        entry_NightBorne.setPosition(490f, 350f)
+        entry_NightBorne.setPosition(490f, 400f)
         stage.addActor(entryTitle_NightBorne)
         entryTitle_NightBorne.setSize(450f, 200f)
-        entryTitle_NightBorne.setPosition(500f, 650f)
+        entryTitle_NightBorne.setPosition(500f, 700f)
+        stage.addActor(entryStats_NightBorne)
+        entryStats_NightBorne.setSize(300f, 250f)
+        entryStats_NightBorne.setPosition(630f, 240f)
+
 
         val styleDictionary = TextButton.TextButtonStyle().apply {
             font = font
